80bf9684d03b3009f2f5ed484a2df9bfc61c8ad1
Steve Iboubi <iboubi@gmail.com>
2013-05-08 16:43:04 -0500

    second commit

---
+++ gen/com/sogeti/siboubib/spingrouponereader/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sogeti.siboubib.spingrouponereader;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
